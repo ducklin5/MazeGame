de29a583b1322893200a41ebb9abf293e8c8c162
ducklin5 <blue-boy-cool@hotmail.com>
2017-08-07 01:43:25 +0300

    added auto server detetction

--- Script/Lobby.gd
+++ Script/Lobby.gd
@@ -1,8 +1,18 @@
 extends Control
 var config = ConfigFile.new()
+var udp = PacketPeerUDP.new()
+const DEFAULT_PORT = 10567
+const MAGIC = "sfjs343"
+var hostsArray = []
 onready var yourIP = find_node("yourIP")
+onready var hostList = get_node("connect/Tabs/Join/hostList")
+onready var joinIP = get_node("connect/Tabs/Join/joinIP")
+
+var my_ip
+var t = Timer.new()
 
 func _ready():
+	#get settings from file
 	var err = config.load("user://settings.cfg")
 	if err != OK or config.get_value("Player","color") == null:
 		config.set_value("Player","name", "Player"+str(randi()))
@@ -11,29 +21,31 @@ func _ready():
 	else:
 		get_node("connect/Tabs/Settings/nick").set_text(config.get_value("Player","name"))
 		get_node("connect/Tabs/Settings/colorPick").set_color(config.get_value("Player","color"))
-		
-	for i in IP.get_local_addresses():
-		if i.find(":") == -1:
-			 yourIP.add_item (i)
+	
+	# get local address
+	my_ip = IP.resolve_hostname(IP.get_local_addresses()[1], 1)
+	yourIP.add_item(my_ip)
+	
+	#connect signals
 	gamestate.connect("player_list_changed", self, "refresh_lobby")
+	
+	#create timer
+	t.set_wait_time(1)
+	add_child(t)
+	t.start()
+	#get game servers on local network
+	
+	refresh_hostList()
 
-func refresh_hostList():
-	var host = NetworkedMultiplayerENet.new()
-	
-	for x in range(168,169):
-		for y in range(8,9):
-			for z in range(100,200):
-				#print("resloving "+str(192)+"."+str(x)+"."+str(y)+"."+str(z))
-				host.create_client(str(192)+"."+str(x)+"."+str(y)+"."+str(z), 10567)
-				host.poll()
-				host.close_connection()
-	
-	
-	
 
 func _on_hostButton_pressed():
 	if (find_node("nick").get_text() == ""):
 		invalidNick()
+		return;
+	udp.close()
+	var err = udp.set_send_address('255.255.255.255',DEFAULT_PORT)
+	if (err != OK):
+		print("Error:\nCan't resolve.")
 	else:
 		var name = find_node("nick").get_text()
 		var color =  find_node("colorPick").get_color()
@@ -41,14 +53,47 @@ func _on_hostButton_pressed():
 		gamestate.host_game(playerInfo, find_node("maxPlayers").get_value())
 		showWaitLobby()
 		refresh_lobby()
-		
+		while(true):
+			var data = [MAGIC,get_node("connect/Tabs/Host/serverName").get_text()]
+			udp.put_var(data)
+			yield(t,"timeout")
+
+func refresh_hostList():
+	var err = udp.listen(DEFAULT_PORT)
+	if (err != OK):
+		print("Error:\nCan't listen.")
+	else:
+		get_node("connect/Tabs/Join/refresh").set_disabled(true)
+		hostsArray = []
+		hostList.clear()
+		var n = 10
+		while n > 0:
+			if udp.get_available_packet_count() > 0:
+				var packet = udp.get_var()
+				var hostIP = udp.get_packet_ip()
+				var port = udp.get_packet_port()
+				
+				if packet[0] == MAGIC and !hostsArray.has(hostIP):
+					hostsArray.append(hostIP)
+					hostList.add_item(packet[1])
+			yield(t,"timeout")
+			n-=1
+		get_node("connect/Tabs/Join/refresh").set_disabled(false)
+
+func _on_hostList_item_selected( index ):
+	var ip = hostsArray[index]
+	var ipFloats = ip.split_floats(".")
+	for i in range (4):
+		joinIP.get_child(i).set_val(ipFloats[i])
+
 func _on_joinButton_pressed():
 	if (find_node("nick").get_text() == ""):
 		invalidNick()
 	else:
-		var joinIP = get_node("connect/Tabs/Join/joinIP")
-		var a = joinIP.get_node("a").get_val(); var b = joinIP.get_node("b").get_val(); var c = joinIP.get_node("c").get_val(); var d = joinIP.get_node("d").get_val()
-		var ip = str(a) + "." + str(b) + "." + str(c) + "." + str(d)
+		var ip = ""
+		for i in range (4):
+			ip += str(joinIP.get_child(i).get_val())
+			if i < 3 : ip += "."
 		var name = find_node("nick").get_text()
 		var color =  find_node("colorPick").get_color()
 		var playerInfo = {"name":name, "color":color}
